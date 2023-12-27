77e679edfadaf6576ca194c6dd2f4d1445c674b2
Palm <palml1lax@gmail.com>
2023-12-27 14:44:43 +0700

    feat: finish ex

--- src/slices/messageBoardSlice.jsx
+++ src/slices/messageBoardSlice.jsx
@@ -1,1 +1,21 @@
 // TODO: Start Create Slice Here
+import { createSlice } from "@reduxjs/toolkit";
+
+export const messageBoardSlice = createSlice({
+  name: "message",
+  initialState: [],
+  reducers: {
+    addMessage: (state, action) => {
+      state = [...state, action.payload];
+      return state;
+    },
+    deleteMessage: (state, action) => {
+      state.splice(action.payload, 1);
+      return state;
+    },
+  },
+});
+
+export const { addMessage, deleteMessage } = messageBoardSlice.actions;
+
+export default messageBoardSlice.reducer;
